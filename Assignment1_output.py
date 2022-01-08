830411e45b14b1e6bf15d9db7f32f224bba32c58
deep-g <97080430+deep-g@users.noreply.github.com>
2022-01-08 22:20:51 +0530

    Add files via upload

---
+++ Assignment1_output.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================================================= RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA1q1.py ========================================================
+Enter 1st number: 24
+Enter 2nd number: 56
+Enter 3rd number: 48
+Average of these numbers =  42.666666666666664
+
+========================================================= RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA1q2.py ========================================================
+Enter Gross income : 24000
+Enter no. of Dependents : 2
+Taxable Income =  8000.0
+Tax =  1600.0
+
+========================================================= RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA1q3.py ========================================================
+Enter your Name : Deepak Gupta
+Enter Student Id : 21103086
+Enter your Gender : M
+Enter the name of your Course : CSE
+Enter your CGPA : 10.0
+
+*****STUDENT DATA*****
+['Deepak Gupta', 21103086, 'M', 'CSE', 10.0]
+
+========================================================= RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA1q4.py ========================================================
+Enter marks of 1st student : 89
+Enter marks of 2nd student : 78
+Enter marks of 3rd student : 98
+Enter marks of 4th student : 46
+Enter marks of 5th student : 96
+Sorted List: [98.0, 96.0, 89.0, 78.0, 46.0]
+
+========================================================= RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA1q5.py ========================================================
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+(a)= ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+ ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+(b)= ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
