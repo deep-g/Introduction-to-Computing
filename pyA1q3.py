b9ea1c220362c316a966cebdd4db37f72dc480e5
deep-g <97080430+deep-g@users.noreply.github.com>
2022-01-08 14:15:26 +0530

    Update pyA1q3.py

--- pyA1q3.py
+++ pyA1q3.py
@@ -6,7 +6,6 @@ sid=int(input("Enter Student Id : "))
 gender=str(input("Enter your Gender : "))
 course=input("Enter the name of your Course : ")
 cgpa=float(input("Enter your CGPA : "))
-
 print("")
 print("*****STUDENT DATA*****")
 student_data = [name , sid , gender , course , cgpa]
