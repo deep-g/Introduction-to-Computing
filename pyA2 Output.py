1bdeeb4a44987f9beee4a474ad235b2498351683
deep-g <97080430+deep-g@users.noreply.github.com>
2022-01-18 10:58:55 +0530

    Add files via upload

---
+++ pyA2 Output.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\pyA2all.py ====
+Question 1:
+Length of string = 35
+String after reversing the order : egaugnal evitisnes esac a si nohtyP
+Sliced string : a case sensitive
+New String : Python is object oriented language
+Index of given substring = 10
+String after removal of white spaces : Pythonisacasesensitivelanguage
+
+Question 2:
+Hey,Deepak Gupta Here!
+My SID is 21103086
+I am from CSE department and my CGPA is 10.0
+
+Question 3:
+a&b = 8
+a|b = 58
+a^b = 50
+a<<2 = 224 and b<<2 = 40
+a>>2 = 14 and b>>4 = 0
+
+Question 4:
+Enter 1st number : 5
+Enter 2nd number : 12
+Enter 3rd number : 34
+34.0 is the largest number
+
+Question 5:
+Input you string : the name of this fruit is banana
+Yes
+
+Question 6:
+Enter 1st side : 12
+Enter 2nd side : 5
+Enter 3rd side : 16
+Yes
