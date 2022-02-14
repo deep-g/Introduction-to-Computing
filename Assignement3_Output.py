fdf6cb46ea298f80647f022ee1ed2d9cf1c24955
deep-g <97080430+deep-g@users.noreply.github.com>
2022-02-14 22:34:36 +0530

    Add files via upload

---
+++ Assignement3_Output.py
@@ -0,0 +1,89 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q1.py =====
+Enter a string :DeepakGupta
+{'D': 1, 'e': 2, 'p': 2, 'a': 2, 'k': 1, 'G': 1, 'u': 1, 't': 1}
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q1.py =====
+Enter a string :Deepak is my name is Deepak
+{'Deepak': 2, 'is': 2, 'my': 1, 'name': 1}
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q2.py =====
+Enter Day :28
+Enter Month :2
+Enter Year :1900
+Next Date is: 01/3/1900
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q2.py =====
+Enter Day :28
+Enter Month :2
+Enter Year :2000
+Next Date is: 29/2/2000
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q2.py =====
+Enter Day :31
+Enter Month :12
+Enter Year :2021
+Next Date is: 01/01/2022
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q3.py =====
+[(1, 1), (5, 25), (7, 49), (9, 81), (10, 100), (14, 196)]
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q4.py =====
+Enter Grade point :7
+Your Grade is 'B' and Good Performance
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q5.py =====
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q6.py =====
+Enter SID of the student :21103086
+Enter name of the student :Deepak Gupta
+To enter more data press Y or press N to stop     Y
+Enter SID of the student :21103087
+Enter name of the student :Jashanjit Singh
+To enter more data press Y or press N to stop     Y
+Enter SID of the student :21103096
+Enter name of the student :Harsh Goyal
+To enter more data press Y or press N to stop     Y
+Enter SID of the student :21103099
+Enter name of the student :Pratham Bansal
+To enter more data press Y or press N to stop     N
+
+a)Dictionary with student details :
+ {21103086: 'Deepak Gupta', 21103087: 'Jashanjit Singh', 21103096: 'Harsh Goyal', 21103099: 'Pratham Bansal'}
+
+b) Dictionary sorted on the basis of name :
+ {21103086: 'Deepak Gupta', 21103096: 'Harsh Goyal', 21103087: 'Jashanjit Singh', 21103099: 'Pratham Bansal'}
+
+c)Dictionary sorted on the basis of SID :
+ {21103086: 'Deepak Gupta', 21103087: 'Jashanjit Singh', 21103096: 'Harsh Goyal', 21103099: 'Pratham Bansal'}
+
+d) Enter SID of student you want to search :21103096
+The name of the student with the entered SID is : Harsh Goyal
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q7.py =====
+Enter the index(n>=2) upto which you want the fibonacci sequence :10
+Resultant Fibonacci sequence is : 0 1 1 2 3 5 8 13 21 34 55 
+Average of this series = 13.00
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q7.py =====
+Enter the index(n>=2) upto which you want the fibonacci sequence :7
+Resultant Fibonacci sequence is : 0 1 1 2 3 5 8 13 
+Average of this series = 4.12
+
+====== RESTART: C:\Users\deepa\OneDrive\Desktop\Deepak\python prog\A3Q8.py =====
+Set1 = {1, 2, 3, 4, 5}
+Set2 = {8, 2, 4, 6}
+Set3 = {1, 17, 5, 9, 13}
+a)Elements that are in Set1 and Set2 but not in both : {1, 3, 5, 6, 8}
+b)Elements that are in only one of Set1,Set2 and Set3 : {3, 6, 8, 9, 13, 17}
+c)Elements that are in exactly two of the sets Set1,Set2 and Set3 : {1, 2, 4, 5}
+d)Integers that are in range 1 to 10 but not in Set1 :	{6, 7, 8, 9, 10}
+e)Integers that are in range 1 to 10 but not in Set1,Set2 and Set3 : {10, 7}
